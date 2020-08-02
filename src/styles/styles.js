fe17ec3831496bb2e7c114594705493e10a0ccf7
erikalbuquerque <eriksilv.77@gmail.com>
2020-08-02 15:36:12 -0300

    adjust the content of the css in the main lights

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const Content = styled.div`
-  position: absolute;
-  top: 0;
-  left: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   padding: 32px;
 `;
 
